44ecc91bb27d5be07b4f659b97202c2cb590dec0
Vidusha <vidushazoro@gmail.com>
2020-09-28 10:56:30 -0700

    Face ID and Map added

--- COVID19/LoginViewController.swift
+++ COVID19/LoginViewController.swift
@@ -7,15 +7,44 @@
 
 import UIKit
 import Firebase
-
+import LocalAuthentication
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-    
+
+    @IBAction func FaceId(_ sender: Any) {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, authenticationError in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        let ac = UIAlertController(title: "Authentication success", message: "Well Done", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "Happy", style: .default))
+                        self?.present(ac, animated: true)
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
+    }
+
 
     @IBOutlet weak var email: UITextField!
     
---
+++ COVID19/ShowMap.swift
@@ -0,0 +1,13 @@
+//
+//  ShowMap.swift
+//  COVID19
+//
+//  Created by Vidusha  on 9/28/20.
+//  Copyright © 2020 Vidusha . All rights reserved.
+//
+
+import UIKit
+
+class ShowMap: NSObject {
+
+}
